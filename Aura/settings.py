0e31231dea0c9893bc2fa91ac7c60f029fa79415
Zameet Sabir <sabirzameet2@gmail.com>
2024-07-30 21:14:52 +0500

    updating smtp password

--- Aura/settings.py
+++ Aura/settings.py
@@ -193,5 +193,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aura.riskdashboard@gmail.com'
-EMAIL_HOST_PASSWORD = 'mgxi eqpz hjok svlz'
+EMAIL_HOST_PASSWORD = 'xzmx zlcn oiyg yxhd'
 EMAIL_PORT = 587
